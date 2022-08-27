8c91a4a35efce2d605172797173c1318ef8f6103
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-08-26 23:14:18 -0700

    Added Scraper For Package Names

--- src/main.rs
+++ src/main.rs
@@ -12,35 +12,50 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut dst = Vec::new();
     let mut easy = Easy::new();
     let args: Vec<String> = env::args().collect();
-    let urlz = format!("https://sourceforge.net/projects/bin-lily/files/{}.tar.xz/download", args[1]);
-    let path = format!("{}.tar.xz", args[1]);
+    if args[1] == "list" {
+        let response = reqwest::blocking::get(
+            "https://sourceforge.net/projects/bin-lily/files/",
+        )
+        .unwrap()
+        .text()
+        .unwrap();
+        
+        let html = scraper::Html::parse_document(&response);
+        let finder = scraper::Selector::parse("tr.file>th>a>span.name").unwrap();
+        let names = html.select(&finder).map(|x| x.inner_html());
+        names
+        .zip(1..101)
+        .for_each(|name| println!("{:?}", name));
+    } else if args[1] == "install" {
+        let urlz = format!("https://sourceforge.net/projects/bin-lily/files/{}.tar.xz/download", args[2]);
+        let path = format!("{}.tar.xz", args[2]);
+        easy.url(&urlz).unwrap();
+        let _redirect = easy.follow_location(true);
+        {
+            let mut transfer = easy.transfer();
+            transfer.write_function(|data| {
+                dst.extend_from_slice(data);
+                Ok(data.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+        {
+            let mut file = File::create(&path)?;
+            file.write_all(dst.as_slice())?;
+        }
 
-    easy.url(&urlz).unwrap();
-    let _redirect = easy.follow_location(true);
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            dst.extend_from_slice(data);
-            Ok(data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
+        Command::new("tar")
+            .arg("-xf")
+            .arg(&path)
+            .spawn()
+            .expect("Command failed to start");
+
+            thread::sleep(time::Duration::from_secs(1));
+
+        Command::new("sh")
+            .arg(format!("{}/install.sh", args[2]))
+            .spawn()
+            .expect("Command failed to start");
     }
-    {
-        let mut file = File::create(&path)?;
-        file.write_all(dst.as_slice())?;
-    }
-
-    Command::new("tar")
-        .arg("-xf")
-        .arg(format!("{}.tar.xz", args[1]))
-        .spawn()
-        .expect("Command failed to start");
-
-        thread::sleep(time::Duration::from_secs(1));
-
-    Command::new("sh")
-        .arg(format!("{}/install.sh", args[1]))
-        .spawn()
-        .expect("Command failed to start");
     Ok(())
 }
